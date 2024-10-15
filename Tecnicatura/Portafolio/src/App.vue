d5a458c89592e49a621c0096c784e0b989f3ac5c
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-10-15 11:10:10 -0300

    Agregando Portafolio VUE

---
+++ Tecnicatura/Portafolio/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import NavBar from './components/icons/NavBar.vue';
+import DatosPersonales from './components/icons/DatosPersonales.vue';
+import EducacionComponente from './components/icons/EducacionComponente.vue';
+import ExperienciaComponente from './components/icons/ExperienciaComponente.vue';
+import ProyectoComponente from './components/icons/ProyectoComponente.vue';
+</script>
+
+<template>
+  <header>
+    <!-- Barra de navegacion -->
+    <NavBar />
+    <!-- Datos personales -->
+    <DatosPersonales />
+  </header>
+  <main>
+    <!-- El estilo css de section se encuentra en base.css -->
+    <section id="educacion">
+      <h2> Educacion - Cursos </h2>
+      <EducacionComponente />
+    </section>
+    <section id="experiencia">
+      <h2> Experiencia </h2>
+      <ExperienciaComponente />
+    </section>
+    <section id="proyectos">
+      <h2> Proyectos </h2>
+      <ProyectoComponente />
+    </section>
+    <section id="habilidades">
+      <h2> Habilidades </h2>
+    </section>
+    <section id="intereses">
+      <h2> Intereses </h2>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+</style>
+ 
---
+++ Tecnicatura/Portafolio/src/main.js
@@ -0,0 +1,7 @@
+// Punto de entrada de nuestro proyecto
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
